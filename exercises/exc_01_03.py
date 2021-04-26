7b40f7868dbf90669d300ab89fcd5406ad555368
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-26 16:46:22 -0400

    feat: Add the printing example to the exc_01_03.

--- exercises/exc_01_03.py
+++ exercises/exc_01_03.py
@@ -1,8 +1,10 @@
 import json
+import os
 
 # This code will run relative to the root of the repo, so we can load files
 with open("exercises/bookquotes.json") as f:
     DATA = json.loads(f.read())
+    print(f.name)
 
 # Print the first record in the DATA
 print(___[____])
