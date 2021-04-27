5249e0c0c9e07ca9f60c70731b6b5ddab992a8d4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-26 20:13:19 -0400

    style: Reformat the source code in the index.js file.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,44 +8,44 @@ import Logo from '../../static/logo.svg'
 import classes from '../styles/index.module.sass'
 
 export default ({ data }) => {
-    const siteMetadata = data.site.siteMetadata
-    const chapters = data.allMarkdownRemark.edges.map(({ node }) => ({
-        slug: node.fields.slug,
-        title: node.frontmatter.title,
-        description: node.frontmatter.description,
-    }))
-    return (
-        <Layout isHome>
-            <Logo className={classes.logo} aria-label={siteMetadata.title} />
+  const siteMetadata = data.site.siteMetadata
+  const chapters = data.allMarkdownRemark.edges.map(({ node }) => ({
+    slug: node.fields.slug,
+    title: node.frontmatter.title,
+    description: node.frontmatter.description,
+  }))
+  return (
+    <Layout isHome>
+      <Logo className={classes.logo} aria-label={siteMetadata.title} />
 
-            <section>
-              <h1 className={classes.subtitle}>Warmups, <em>for Developers</em></h1>
-                <div className={classes.introduction}>
+      <section>
+        <h1 className={classes.subtitle}>Warmups, <em>for Developers</em></h1>
+        <div className={classes.introduction}>
 
-                <p> If you are like many developers, you may find it frustrating when you are starting to program
-                for the first time &mdash; or returning to your code after the weekend. 😉 Whether you are
-                procrastinating or overwhelmed by a task, <b>warmups.dev</b> can help you to review basic programming
-                skills and rev up your mind. Let's go! 🚀</p>
+          <p> If you are like many developers, you may find it frustrating when you are starting to program for
+          the first time &mdash; or returning to your code after the weekend. <span role="img"
+          aria-label="wink">😉</span> Whether you are procrastinating or overwhelmed by a task, <b>warmups.dev</b>
+        can help you to review basic programming skills and rev up your mind. Let's go! 🚀</p>
 
-                </div>
-            </section>
+    </div>
+  </section>
 
-            {chapters.map(({ slug, title, description }) => (
-                <section key={slug} className={classes.chapter}>
-                    <h2 className={classes.chapterTitle}>
-                        <Link hidden to={slug}>
-                            {title}
-                        </Link>
-                    </h2>
-                    <p className={classes.chapterDesc}>
-                        <Link hidden to={slug}>
-                            {description}
-                        </Link>
-                    </p>
-                </section>
-            ))}
-        </Layout>
-    )
+  {chapters.map(({ slug, title, description }) => (
+    <section key={slug} className={classes.chapter}>
+      <h2 className={classes.chapterTitle}>
+        <Link hidden to={slug}>
+          {title}
+        </Link>
+      </h2>
+      <p className={classes.chapterDesc}>
+        <Link hidden to={slug}>
+          {description}
+        </Link>
+      </p>
+    </section>
+  ))}
+  </Layout>
+  )
 }
 
 export const pageQuery = graphql`
