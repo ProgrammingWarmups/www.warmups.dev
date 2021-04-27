bb4ae7ea1138774d911653de69b2bacb9ac53e4f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-27 13:54:36 -0400

    fix: Make the Logo in layout.js for !isHome to be square.

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,7 +37,7 @@ const Layout = ({ isHome, title, description, children }) => {
                             {!isHome && (
                                 <h1 className={classes.logo}>
                                     <Link hidden to="/">
-                                        <Logo width={150} height={75} aria-label={meta.title} />
+                                        <Logo width={75} height={75} aria-label={meta.title} />
                                     </Link>
                                 </h1>
                             )}
