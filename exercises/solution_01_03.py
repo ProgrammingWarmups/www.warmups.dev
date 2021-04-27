1240173ac889ccd30e793c684bfb08830a6a623c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-27 15:08:41 -0400

    feat: Add correct solution_01_03.py.

--- exercises/solution_01_03.py
+++ exercises/solution_01_03.py
@@ -1,13 +1,10 @@
-import json
-import os
-
-# This code will run relative to the root of the repo, so we can load files
-with open("exercises/bookquotes.json") as f:
-    DATA = json.loads(f.read())
-    print(f.name)
-
-# Print the first record in the DATA
-print(DATA[0])
-
-# Assign the length of DATA to some_var
-some_var = len(DATA)
+# create a list of numbers
+numbers = [2, 4, 6, 8]
+# add the numbers together
+s = sum(numbers)
+# count the values in numbers
+N = len(numbers)
+# compute the arithmetic mean
+mean = s / N
+# display the arithmetic mean
+print(f"Mean: {mean}")
