10007a6b94f2cd9726e14a4e1f9d5d71a0de820a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-27 10:51:35 -0400

    feat: Use correct label for a chapter, now as "Warmup" in chapter.js.

--- src/templates/chapter.js
+++ src/templates/chapter.js
@@ -18,8 +18,8 @@ const Template = ({ data }) => {
     const [completed, setCompleted] = useLocalStorage(`${courseId}-completed-${id}`, [])
     const html = renderAst(htmlAst)
     const buttons = [
-        { slug: prev, text: '« Previous Chapter' },
-        { slug: next, text: 'Next Chapter »' },
+        { slug: prev, text: '« Previous Warmup' },
+        { slug: next, text: 'Next Warmup »' },
     ]
 
     return (
