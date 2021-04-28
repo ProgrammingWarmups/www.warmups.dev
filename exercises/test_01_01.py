8ab6da7197bfd569f212cab00ef7e9bc6844b954
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-28 10:54:04 -0400

    refactor: Rewrite the test_01_01 to fit new approach with no slides.

--- exercises/test_01_01.py
+++ exercises/test_01_01.py
@@ -1,9 +1,8 @@
 def test():
     # if an assertion fails, the message will be displayed
-    # --> must calculate the arithmetic mean
-    assert "mean = s / N" in __solution__, "Are you calculating the arithmetic mean?"
-    assert mean == s / N, "Are you calculating the arithmetic mean?"
+    # --> must have the output in a comment
+    assert "Mean: 5.0" in __solution__, "Did you record the correct program output as a comment?"
     # --> must display the arithmetic mean
-    assert "{mean}" in __solution__, "Are you displaying the arithmetic mean?"
+    assert "DONE" in __solution__, "Did you mark the TODO task as DONE?"
     # display a congratulations for a correct solution
     __msg__.good("Well done!")
