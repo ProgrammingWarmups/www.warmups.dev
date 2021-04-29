250d7db93a4c0118e577f8e3bf1329f19922a94f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-29 12:08:45 -0400

    refactor: Improve the assertion in the test_01_01 to fit new intention.

--- exercises/test_01_01.py
+++ exercises/test_01_01.py
@@ -3,6 +3,6 @@ def test():
     # --> must have the output in a comment
     assert "Mean: 5.0" in __solution__, "Did you record the correct program output as a comment?"
     # --> must display the arithmetic mean
-    assert "DONE" in __solution__, "Did you mark the TODO task as DONE?"
+    assert "TODO" not in __solution__, "Did you remove the TODO marker when finished?"
     # display a congratulations for a correct solution
     __msg__.good("Well done!")
