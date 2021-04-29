cd94e505147153979769f367d0d14f2be671db09
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-29 19:49:48 -0400

    fix: Add the correctly jumbled code in the exc_01_03.

--- exercises/exc_01_03.py
+++ exercises/exc_01_03.py
@@ -1,9 +1,10 @@
+# TODO: fix all the mistakes
 # create a list of numbers
 numbers = [2, 4, 6, 8]
 # add the numbers together
-s = sum(numbers)
+s = len(numbers)
 # count the values in numbers
-N = len(numbers)
+N = sum(numbers)
 # compute the arithmetic mean
 mean = s / N
 # display the arithmetic mean
