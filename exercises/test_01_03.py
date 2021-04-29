be7ff04bccdee90a69d8943d2dfff25ff8fc90df
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-29 19:51:35 -0400

    feat: Add all of the correct checks for the test_01_03.

--- exercises/test_01_03.py
+++ exercises/test_01_03.py
@@ -1,8 +1,16 @@
 def test():
     # if an assertion fails, the message will be displayed
-    # --> must have the output in a comment
-    assert "Mean: 5.0" in __solution__, "Did you record the correct program output as a comment?"
+    # --> must calculate the arithmetic mean as 4.0
+    assert mean == 4.0, "Are you correctly calculating the arithmetic mean?"
+    # --> must use the correct equation for calculating arithmetic mean
+    assert "s / N" in __solution__, "Are you correctly calculating the arithmetic mean?"
     # --> must display the arithmetic mean
-    assert "DONE" in __solution__, "Did you mark the TODO task as DONE?"
+    assert "{mean}" in __solution__, "Are you displaying the arithmetic mean?"
+    # --> must use the correct assignment for sum
+    assert "s = sum(" in __solution__, "Are you correctly calculating the sum?"
+    # --> must use the correct assignment for len
+    assert "N = len(" in __solution__, "Are you correctly calculating the length?"
+    # --> must not have a TODO marker in the solution
+    assert "TODO" not in __solution__, "Did you remove the TODO marker when finished?"
     # display a congratulations for a correct solution
     __msg__.good("Well done!")
