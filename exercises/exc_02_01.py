6bfdba3c513d1c742497264d4eeef6f5f76e0a63
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-05-04 14:00:03 -0400

    feat: Add the starting version of the exc_02_01.

---
+++ exercises/exc_02_01.py
@@ -0,0 +1,11 @@
+def mean(numbers):
+    # create a list of numbers
+    numbers = [2, 4, 6, 8]
+    # add the numbers together
+    s = sum(numbers)
+    # count the values in numbers
+    N = len(numbers)
+    # compute the arithmetic mean
+    mean = s / N
+    return mean
+
