23c055079ce838b16cbb6523a46fb788c2026f0d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-05-04 19:44:38 -0400

    feat: Add the full version of the exc_02_01 to copy to solution.

--- exercises/exc_02_01.py
+++ exercises/exc_02_01.py
@@ -1,6 +1,5 @@
 def mean(numbers):
-    # create a list of numbers
-    numbers = [2, 4, 6, 8]
+    """Compute the arithmetic mean of the values in numbers."""
     # add the numbers together
     s = sum(numbers)
     # count the values in numbers
@@ -9,3 +8,14 @@ def mean(numbers):
     mean = s / N
     return mean
 
+# create a list of numbers
+numbers_one = [1, 3, 5, 7]
+# calculate the mean
+numbers_one_mean = mean(numbers_one)
+print(f"Mean: {numbers_one_mean}")
+
+# create a list of numbers
+numbers_two = [2, 4, 6, 8]
+# calculate the mean
+numbers_two_mean = mean(numbers_two)
+print(f"Mean: {numbers_two_mean}")
