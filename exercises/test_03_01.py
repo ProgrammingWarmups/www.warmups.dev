403d26381a58e2d476f2d00718b2dd4a93766f87
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-05-05 17:12:29 -0400

    feat: Finish adding all of the checks to the test_03_01.

--- exercises/test_03_01.py
+++ exercises/test_03_01.py
@@ -2,6 +2,10 @@ def test():
     # if an assertion fails, the message will be displayed
     # --> must have the diagnostic comment
     assert "10 email addresses" in __solution__, "Did you record the correct program output as a comment?"
+    # --> must have code construct
+    assert "print(file_contents)" in __solution__, "Did you print the file_contents variable?"
+    # --> must have code construct
+    assert "display(emails_text)" in __solution__, "Did you display the emails_text variable?"
     # --> must not have a TODO marker in the solution
     assert "TODO" not in __solution__, "Did you remove the TODO marker when finished?"
     # display a congratulations for a correct solution
