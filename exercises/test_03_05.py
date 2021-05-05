5e3a242631f7fd958575695f6951165a63831d24
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-05-05 18:44:47 -0400

    feat: Add further checks to the test_03_05.

--- exercises/test_03_05.py
+++ exercises/test_03_05.py
@@ -1,5 +1,13 @@
 def test():
     # if an assertion fails, the message will be displayed
+    # --> must have code construct
+    assert "read(emails_file)" in __solution__, "Did you call the read function correctly?"
+    # --> must have code construct
+    assert "search(emails_text" in __solution__, "Did you call the search function correctly?"
+    # --> must have code construct
+    assert "search(emails_text" in __solution__, "Did you call the search function correctly?"
+    # --> must have code construct
+    assert "display(matching_emails)" in __solution__, "Did you call the display function correctly?"
     # --> must not have a TODO marker in the solution
     assert "TODO" not in __solution__, "Did you remove the TODO marker when finished?"
     # display a congratulations for a correct solution
