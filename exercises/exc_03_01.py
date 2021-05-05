3e6cce215bbfaca29ac76d72673ab249c0b16542
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-05-05 16:21:52 -0400

    feat: Add more debugging code to exc_03_01.

--- exercises/exc_03_01.py
+++ exercises/exc_03_01.py
@@ -7,6 +7,15 @@ def read(filename):
 def display(contents):
     print(contents)
 
+def print_tree(directory):
+    print(f'+ {directory}')
+    for path in sorted(directory.rglob('*')):
+        depth = len(path.relative_to(directory).parts)
+        spacer = '    ' * depth
+        print(f'{spacer}+ {path.name}')
+
+print_tree(pathlib.Path("exercises"))
+
 emails_file = "exercises/emails.txt"
 emails_text = read(emails_file)
 display(emails_text)
