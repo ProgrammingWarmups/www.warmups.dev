49174a1a49cedcd26525210d8f98e4ac01a2274f
Maria Kim <yeeunmariakim@gmail.com>
2021-05-12 19:02:09 -0400

    Make error message more specific

--- exercises/test_03_01.py
+++ exercises/test_03_01.py
@@ -1,7 +1,7 @@
 def test():
     # if an assertion fails, the message will be displayed
     # --> must have the diagnostic comment
-    assert "10 email addresses" in __solution__, "Did you record the correct program output as a comment?"
+    assert "10 email addresses" in __solution__, "Did you record the correct number of emails as a comment?"
     # --> must have code construct
     assert "print(file_contents)" in __solution__, "Did you print the file_contents variable?"
     # --> must have code construct
